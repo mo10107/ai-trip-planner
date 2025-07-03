b1863fd5f3d881d3e95204b6b74f677ef3dba658
mo10107 <67790368+mo10107@users.noreply.github.com>
2025-07-03 16:19:40 +0300

    created endpoints with frontend

--- agent/agent_workflow.py
+++ agent/agent_workflow.py
@@ -1,16 +1,23 @@
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from prompt_library.prompt import SYSTEM_PROMPT
-
+from utils.model_loader import ModelLoader
 class GraphBuilder():
-    def __init__(self):
-        self.tools = []
+    def __init__(self, model_provider: str ="together_ai"):
         self.system_prompt = SYSTEM_PROMPT
-    
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+
+        self.tools = []
+
+
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        self.graph = None
+        
     def agent_function(self, state: MessagesState):
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
-        response = self.llm_with_tools_invoke(input_question)
+        response = self.llm_with_tools.invoke(input_question)
         return {"messages": [response]}
 
     def build_graph(self):
--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from ai-trip-planner!")
+from fastapi import FastAPI, JsonResponse
+from fastapi.middleware.cors import CORSMiddleware
+from agent.agent_workflow import GraphBuilder
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class QueryRequest(BaseModel):
+    query: str = Field(..., description="The query to be processed by the agent.")
+
+
+@app.post("/query")
+async def process_travel_agent(query: QueryRequest):
+    try:
+        graph = GraphBuilder(model_provider="together_ai")
+        react_app = graph()
+
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open("graph.png", "wb") as f:
+            f.write(png_graph)
+
+        print(f"Graph saved as graph.png at {os.getcwd()}")
+        messages = {"messages": [query.query]}
+        response = react_app.invoke(messages)
+
+        if isinstance(response, dict) and "messages" in response:
+            response_message = response["messages"][-1].content
+        else:
+            response_message = str(response)
+
+        return JsonResponse(
+            {"response": response_message, "status": "success"}, status=200
+        )
+    
+    except Exception as e:
+        return JsonResponse({"error": str(e), "status": "error"}, status=500)
